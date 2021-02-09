caebdec6570eee77a41a516e4c66d08c143352a0
Katelinn Jones <katiejonesyo@gmail.com>
2021-02-08 22:33:33 -0800

    avatar container and partial app.jsx done

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+     <Router>
+      <Switch>
+        <Route exact path="/" component={() => <AvatarContainer />} />
+        <Route exact path="/detail/:id" component={DetailPage} />
+      </Switch>
+    </Router>
+  );
 }
+
+
